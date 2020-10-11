14e4e59c337de04f0f2beac24ac8af48db264cd7
Thomas Willson <thomas.willson@me.com>
2020-10-10 18:35:26 -0700

    Initial creation of macOS and iOS apps.

---
+++ src/apple-platforms/WeathAir/WeathAir-macOS/WeathAir-macOS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  WeathAir-macOS
+//
+//  Created by Thomas Willson on 2020-10-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
