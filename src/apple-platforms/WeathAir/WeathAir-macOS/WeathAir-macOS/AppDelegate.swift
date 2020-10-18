6b0b56a509aa1ad8f858aea81414e1c5a7be2191
Thomas Willson <thomas.willson@me.com>
2020-10-17 17:18:06 -0700

    Additional visual tweaks.

--- src/apple-platforms/WeathAir/WeathAir-macOS/WeathAir-macOS/AppDelegate.swift
+++ src/apple-platforms/WeathAir/WeathAir-macOS/WeathAir-macOS/AppDelegate.swift
@@ -13,20 +13,20 @@ class CXETextLayer : CATextLayer {
 	override init() {
 		super.init()
 	}
-
+	
 	override init(layer: Any) {
 		super.init(layer: layer)
 	}
-
+	
 	required init(coder aDecoder: NSCoder) {
 		super.init(layer: aDecoder)
 	}
-
+	
 	override func draw(in ctx: CGContext) {
 		let height = self.bounds.size.height
 		let fontSize = self.fontSize
 		let yDiff = (height-fontSize)/2 - fontSize/10
-
+		
 		ctx.saveGState()
 		ctx.translateBy(x: 0.0, y: yDiff)
 		super.draw(in: ctx)
@@ -35,25 +35,29 @@ class CXETextLayer : CATextLayer {
 }
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-
+class AppDelegate: NSObject, NSApplicationDelegate, NSPopoverDelegate {
+	
 	var window: NSWindow!
-    var popover: NSPopover!
-    var statusBarItem: NSStatusItem!
-    var sub : Any?
-    
-    @State var observation: Observation?
+	var popover: NSPopover!
+	var statusBarItem: NSStatusItem!
+	var sub : Any?
+	var caLayer: CALayer?
+	var textLayer: CATextLayer?
+	var textBorderColor: CGColor?
+	
+	@State var observation: Observation?
 	
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Create the SwiftUI view that provides the window contents.
 		let viewModel = ViewModel()
 		let contentView = ContentView(viewModel)
-
-        let popover = NSPopover()
-        popover.contentSize = NSSize(width: 200, height: 100)
-        popover.behavior = .transient
-        popover.contentViewController = NSHostingController(rootView: contentView)
-        self.popover = popover
+		
+		let popover = NSPopover()
+		popover.contentSize = NSSize(width: 200, height: 100)
+		popover.behavior = .transient
+		popover.contentViewController = NSHostingController(rootView: contentView)
+		popover.delegate = self
+		self.popover = popover
 		
 		self.statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 		let button = self.statusBarItem.button!
@@ -63,20 +67,24 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		
 		let layer = CALayer()
 		layer.contentsGravity = CALayerContentsGravity.resizeAspectFill
-		layer.cornerRadius = 8.0
-		layer.masksToBounds = true
-		layer.backgroundColor = NSColor(calibratedRed: 0.0, green: CGFloat(0xe4) / 255, blue: 00, alpha: 1.0).cgColor
+		
 		button.layer = layer
 		
-		let textLayer = CXETextLayer()
-		textLayer.string = "WeathAir"
-		textLayer.frame = layer.bounds
-		textLayer.fontSize = 14.0
-		textLayer.foregroundColor = NSColor.black.cgColor
-		textLayer.font = NSFont.systemFont(ofSize: 14.0, weight: .medium)
-		textLayer.alignmentMode = .center
-
-		layer.addSublayer(textLayer)
+		textLayer = CXETextLayer()
+		textLayer!.cornerRadius = 10.0
+		textLayer!.masksToBounds = true
+		textLayer!.backgroundColor = NSColor(calibratedRed: 0.0, green: CGFloat(0xe4) / 255, blue: 00, alpha: 1.0).cgColor
+		textLayer!.borderWidth = 3.0
+		textLayer!.string = "WeathAir"
+		textLayer!.frame = layer.bounds
+		textLayer!.fontSize = 12.0
+		textLayer!.foregroundColor = NSColor.black.cgColor
+		textLayer!.font = NSFont.systemFont(ofSize: 12.0, weight: .bold)
+		textLayer!.alignmentMode = .center
+		textBorderColor = textLayer!.borderColor
+		
+		layer.addSublayer(textLayer!)
+		self.caLayer = layer
 		
 		sub = contentView.viewModel
 			.objectWillChange
@@ -84,30 +92,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 			.sink { () in
 				if let observation = contentView.viewModel.observation {
 					button.title = "AQI \(observation.aqiValue ?? -1)"
-					layer.backgroundColor = observation.color
+					self.textLayer!.backgroundColor = observation.color
 				} else {
 					button.title = "WeathAir"
-					layer.backgroundColor = CGColor.clear
+					self.textLayer!.backgroundColor = self.textBorderColor
 				}
-
-				textLayer.frame = button.frame
-				textLayer.string = button.title
+				
+				self.textLayer!.frame = button.frame
+				self.textLayer!.string = button.title
 			}
 	}
-    
-    @objc func togglePopover(_ sender: AnyObject?) {
-         if let button = self.statusBarItem.button {
-              if self.popover.isShown {
-                   self.popover.performClose(sender)
-              } else {
-                   self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-              }
-         }
-    }
-
+	
+	@objc func togglePopover(_ sender: AnyObject?) {
+		if let button = self.statusBarItem.button {
+			if self.popover.isShown {
+				self.popover.performClose(sender)
+			} else {
+				let color = NSColor.selectedMenuItemColor.cgColor
+				self.textLayer?.borderColor = color
+				self.caLayer?.backgroundColor = color
+				self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+			}
+		}
+	}
+	
 	func applicationWillTerminate(_ aNotification: Notification) {
 		// Insert code here to tear down your application
 	}
-
-
+	
+	func popoverWillClose(_ notification: Notification) {
+		self.caLayer?.backgroundColor = NSColor.clear.cgColor
+		self.textLayer?.borderColor = textBorderColor
+	}
 }
